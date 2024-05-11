d70f1d83fbf69e4df0dbe94907234b1433bb8174
joao pedro matos <jpedro030506@gmail.com>
2024-05-11 20:50:22 -0300

    .

--- calculator.py
+++ calculator.py
@@ -24,10 +24,10 @@ entry.pack(fill=X, ipadx=8, pady=10, padx=10)
 button_frame = Frame(root)
 button_frame.pack()
 
-list_of_buttons = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '.', '0', '=', '+', 'C']
+lista_de_botoes = ['7', '8', '9', '/', '4', '5', '6', '*', '1', '2', '3', '-', '.', '0', '=', '+', 'C']
 
 i = 0
-for button_text in list_of_buttons:
+for button_text in lista_de_botoes:
     button = Button(button_frame, text=button_text, font='lucida 15 bold')
     button.grid(row=int(i/4), column=i%4, padx=3, pady=3)
     button.bind("<Button-1>", click)
